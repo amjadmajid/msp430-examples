21bd38612b59d21661bebc34e18365d9510a00c5
amjadmajid <amjad.y.majid@gmail.com>
2017-02-12 20:48:33 +0100

    Set the MCLK =DCO and set the DCO to 2.67 MHz

---
+++ set_clock_01/Debug/main.pp
@@ -0,0 +1,13 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/ti/ccsv6/ccs_base/msp430/include/msp430fr5969.h
+main.obj: /Applications/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+/Applications/ti/ccsv6/ccs_base/msp430/include/msp430fr5969.h: 
+/Applications/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics_legacy_undefs.h: 
